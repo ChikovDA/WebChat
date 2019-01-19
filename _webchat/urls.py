a66f0e4b105601c063094b0c1641d1061820045a
bars <blooodchik@yandex.ru>
2019-01-19 05:19:44 +0500

    добавлен сам чат, но он не работает :(

--- _webchat/urls.py
+++ _webchat/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^', include('publicroom.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^chat/', include('chatrooms.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+
+
